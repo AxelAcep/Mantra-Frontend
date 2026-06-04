54b801c7266c16f9ac12c59b773cdd3b3a947fed
Axel <axelsebayang456@gmail.com>
2026-06-04 21:04:47 +0700

    resolve error

--- src/hooks/useDetailTrackingPenawaran.ts
+++ src/hooks/useDetailTrackingPenawaran.ts
@@ -1,13 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import {
-  getDetailTrackingPenawaran,
-  type TrackingPenawaranDetail,
-} from "@/services/tracking-penawaran.service";
+// import { useQuery } from "@tanstack/react-query";
+// import {
+//   getDetailTrackingPenawaran,
+//   type TrackingPenawaranDetail,
+// } from "@/services/tracking-penawaran.service";
 
-export function useDetailTrackingPenawaran(id: string) {
-  return useQuery({
-    queryKey: ["tracking-penawaran", id],
-    queryFn: () => getDetailTrackingPenawaran(id),
-    enabled: !!id,
-  });
-}
+// export function useDetailTrackingPenawaran(id: string) {
+//   return useQuery({
+//     queryKey: ["tracking-penawaran", id],
+//     queryFn: () => getDetailTrackingPenawaran(id),
+//     enabled: !!id,
+//   });
+// }
